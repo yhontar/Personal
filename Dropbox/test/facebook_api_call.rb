cd182305a16d854f46dd8998f867825be1945bb9
Vlad <tsymbalenkovlad@gmail.com>
2015-07-08 15:48:40 +0300

    Now auto-create database
    mysql2 gem instead mysql

--- Dropbox/test/facebook_api_call.rb
+++ Dropbox/test/facebook_api_call.rb
@@ -1,22 +1,25 @@
 require 'koala'
 require 'faker'
-require 'mysql'
+require 'mysql2'
 
-#token = 'https://graph.facebook.com/oauth/access_token?client_id=899768646747265&client_secret=a24d6671f94ce9eff9d79f39410df147&grant_type=client_credentials'
 oauth = Koala::Facebook::OAuth.new(899768646747265, 'a24d6671f94ce9eff9d79f39410df147')
 token = oauth.get_app_access_token
 graph = Koala::Facebook::API.new(token)
-user = graph.get_object('4?')
-pic = graph.get_object('4/picture?width=800&height=800&redirect=false')['data']
 
 begin
-  fb_api = Mysql.new 'localhost', 'user', 'password', 'test_ruby'
+  fb_api = Mysql2::Client.new(host:"localhost", username:"root", password:"iamyourfather")
+  fb_api.query("CREATE DATABASE IF NOT EXISTS test_results")
+  fb_api = Mysql2::Client.new(host:"localhost", username:"root", password:"iamyourfather", database:"test_results")
   fb_api.query("CREATE TABLE IF NOT EXISTS \
         Facebook_API(Name TEXT, Picture TEXT, Image TEXT)")
-  fb_api.query("INSERT INTO Facebook_API(Name) VALUES('#{user['name']}')")
-  fb_api.query("INSERT INTO Facebook_API(Picture) VALUES('#{pic['url']}')")
-  fb_api.query("INSERT INTO Facebook_API(Image) VALUES(\"#{Faker::Internet.email}\")")
-  rs = fb_api.query("SELECT * FROM   Facebook_API")
-  n_rows = rs.num_rows
-  n_rows.times {puts rs.fetch_row.join("\s")}
+  fb_api.query("INSERT INTO Facebook_API(Name) VALUES('#{ graph.get_object('4?')['name']}')")
+  fb_api.query("INSERT INTO Facebook_API(Picture) VALUES('#{graph.get_object('4/picture?width=800&height=800&redirect=false')['data']['url']}')")
+  fb_api.query("INSERT INTO Facebook_API(Image) VALUES('#{Faker::Internet.email}')")
+  results = fb_api.query("SELECT * FROM Facebook_API")
+  results.each do |row|
+    puts row["Name"]
+    puts row["Picture"]
+    puts row["Image"]
+  end
 end
+
