9f9e6cd6486b240e94f32c56a8c654a7c0e7dfeb
Vlad <tsymbalenkovlad@gmail.com>
2015-07-08 15:49:24 +0300

    Now auto-create database
    Mysql2 gem instead of mysql

--- Dropbox/test/parser.rb
+++ Dropbox/test/parser.rb
@@ -1,12 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
-require 'mysql'
+require 'mysql2'
 
 page = Nokogiri::HTML(open('http://www.bbc.com/news'))
-
+#File.new "/home/vlad/out.txt", "w"
 items = page.css('.distinct-component-group').collect {|block| block}
 
-items.each  do |part| 
+items.each  do |part|
   if part.css('.group-title').text.include? "Features & Analysis" then
     $titles = part.css('.title-link__title-text')
     $dates = part.css('.mini-info-list__item .date.date--v2')
@@ -15,15 +15,22 @@ items.each  do |part|
   end
 end
 
-begin 
-  result = Mysql.new 'localhost', 'user', 'password', 'parse_result'
+begin
+  result = Mysql2::Client.new(host:"localhost", username:"root", password:"iamyourfather")
+  result.query("CREATE DATABASE IF NOT EXISTS test_results")
+  result = Mysql2::Client.new(host:"localhost", username:"root", password:"iamyourfather", database:"test_results")
   result.query("CREATE TABLE IF NOT EXISTS \
         Result(Title TEXT, Summary TEXT, Image TEXT, Date TEXT)")
-  $titles.each {|title| result.query("INSERT INTO Result(Title) VALUES('#{title.text}')")}
+  $titles.each {|title| result.query("INSERT INTO Result(Title) VALUES('#{title.text.delete(?').tr ' ', ?_}')")}
   $images.each {|image| result.query("INSERT INTO Result(Image) VALUES('#{image['data-src']}')")}
   $descriptions.each {|descr| result.query("INSERT INTO Result(Summary) VALUES(\"#{descr.text}\")")}
   $dates.each {|date| result.query("INSERT INTO Result(Date) VALUES('#{date.text}')")}
-  rs = result.query("SELECT * FROM Result")
-  n_rows = rs.num_rows        
-  n_rows.times {puts rs.fetch_row.join("\s")}
+  results = result.query("SELECT * FROM Result")
+  results.each do |row|
+    puts row["Title"]
+    puts row["Desritption"]
+    puts row["Image"]
+    puts row["Date"]
+  end
 end
+
